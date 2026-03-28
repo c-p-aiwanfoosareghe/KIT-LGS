284064782b344cd5ec747d9449fde10dec9322ac
c-p-aiwanfoosareghe <aiwanfoosareghe@gmail.com>
2026-03-28 05:52:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const usernameEl = document.getElementById('username');
 const avatarEl = document.getElementById('avatar');
 
 // Your TikTok App credentials
-const CLIENT_KEY = 'awjhx15ww6w7rqmx';
-const REDIRECT_URI = 'https://c-p-aiwanfoosareghe.github.io/KIT-LGS/callback.html'; // must match TikTok app
+const CLIENT_KEY = 'sbawh4szh02u1o82ta';
+const REDIRECT_URI = 'https://c-p-aiwanfoosareghe.github.io/KIT-LGS/callback.html/'; // must match TikTok app
 
 loginBtn.addEventListener('click', () => {
   const authURL = `https://www.tiktok.com/auth/authorize/?client_key=${CLIENT_KEY}&scope=user.info.basic&response_type=code&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&state=demo`;
